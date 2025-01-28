1b36bcfc679efe41616616a1d6d2763b28646b48
Muli Job <munyoki912@gmail.com>
2025-01-28 20:17:52 +0300

    Added movie model and updated requirements.txt file

--- backend/movie/admin.py
+++ backend/movie/admin.py
@@ -1,3 +1,7 @@
+"""Admin Views"""
 from django.contrib import admin
 
+from .models import Movie
+
 # Register your models here.
+admin.site.register(Movie)
--- backend/movie/models.py
+++ backend/movie/models.py
@@ -1,3 +1,17 @@
+"""Models for the movie application"""
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """Movies Classes"""
+    title = models.CharField(max_length=20, blank=False)
+    poster = models.ImageField(upload_to ="upload/poster/", blank=False)
+    genre = models.CharField(max_length=20, blank=False)
+    actor = models.CharField(max_length=20, blank=False)
+    description = models.TextField(blank=False)
+    year = models.IntegerField(blank=False)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
