bcff8c61d767f593454b299fbc51fef498c5296f
Muli Job <munyoki912@gmail.com>
2025-01-29 10:07:25 +0300

    added user model serializer

--- backend/movie/serializers.py
+++ backend/movie/serializers.py
@@ -1,7 +1,15 @@
 """Movie Serializers"""
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from .models import Movie
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """User Registration"""
+    class Meta:
+        """Registration Fields"""
+        model = User
+        fields = ['id', 'username', 'password']
 
 class MovieSerializer(serializers.ModelSerializer):
     """Movie Serializer"""
